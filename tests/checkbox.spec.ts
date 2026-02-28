5da1704581590007c4a6f4b4e6f678fdfca42e24
Ngô Thị Thu Hương <ntthu@netcompany.com>
2026-02-28 16:23:26 +0700

    1st commit

---
+++ tests/checkbox.spec.ts
@@ -0,0 +1,20 @@
+// Test case =fx( Arrange, Act, Asset)
+
+// test('get started link works', async ({ page }) => {
+// await page.goto('https://the-internet.herokuapp.com/checkboxes');
+// await page.getByRole('checkbox').first();
+// await expect(page.getByRole('checkbox').first()).toBeChecked();
+// })
+
+import { test, expect } from '@playwright/test';
+
+test('verify checkbox', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/checkboxes');
+
+    await page.getByRole('checkbox').nth(1)
+    await expect(page.getByRole('checkbox').nth(1)).toBeChecked();
+
+    await page.getByRole('checkbox').first()
+    await expect(page.getByRole('checkbox').first()).toBeChecked();
+
+});
